9f93407f2e4240533fbf68b5502d8732859441f3
Josh <joshuatylermartinez@gmail.com>
2023-02-15 22:31:42 -0500

    Added Header with default text Added Chicken.png to the src folder Displayed chicken.png Added Tortoise list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import chicken from "./chicken.png";
 
 function App(): JSX.Element {
     return (
@@ -7,6 +8,16 @@ function App(): JSX.Element {
             <header className="App-header">
                 Joshua Martinez UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>This is header text</h1>
+            <img src={chicken} alt="CHICKEN" />
+            <br></br>
+            <br></br>
+            My three Tortoises:
+            <ul>
+                <li>Hershey</li>
+                <li>Maul</li>
+                <li>Yoda</li>
+            </ul>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
