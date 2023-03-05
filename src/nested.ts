b496a37e19892f5de58feb2fcc86be55f16c105b
Josh <joshuatylermartinez@gmail.com>
2023-03-05 17:13:44 -0500

    Fixed sum Points 10 left

--- src/nested.ts
+++ src/nested.ts
@@ -66,7 +66,11 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const sum = questions.reduce(
+        (currentTotal: number, question: Question) => currentTotal + question.points,
+        0
+    );
+    return sum;
 }
 
 /***
