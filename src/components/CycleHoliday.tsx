7b714d89ba30b2c2564cacc57db303c7b9337720
Josh <joshuatylermartinez@gmail.com>
2023-03-12 20:40:57 -0400

    CycleHoliday.tsx

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,38 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<
+        "NewYears" | "MyBirthday" | "Christmas" | "Thanksgiving" | "Easter"
+    >("NewYears");
+    function nextHolidayAlpha() {
+        holiday === "Christmas" ? setHoliday("Easter") : null;
+        holiday === "Easter" ? setHoliday("MyBirthday") : null;
+        holiday === "MyBirthday" ? setHoliday("NewYears") : null;
+        holiday === "NewYears" ? setHoliday("Thanksgiving") : null;
+        holiday === "Thanksgiving" ? setHoliday("Christmas") : null;
+    }
+    function nextHolidayChron() {
+        holiday === "NewYears" ? setHoliday("Easter") : null;
+        holiday === "Easter" ? setHoliday("MyBirthday") : null;
+        holiday === "MyBirthday" ? setHoliday("Thanksgiving") : null;
+        holiday === "Thanksgiving" ? setHoliday("Christmas") : null;
+        holiday === "Christmas" ? setHoliday("NewYears") : null;
+    }
+    return (
+        <div>
+            <Button onClick={nextHolidayAlpha}>Advance by Alphabet</Button>
+            <Button onClick={nextHolidayChron}>Advance by Year</Button>
+            {holiday === "NewYears" ? (
+                <div>Holiday: 🎉</div>
+            ) : holiday === "MyBirthday" ? (
+                <div>Holiday: 🎂</div>
+            ) : holiday === "Easter" ? (
+                <div>Holiday: 🪺</div>
+            ) : holiday === "Thanksgiving" ? (
+                <div>Holiday: 🦃</div>
+            ) : holiday === "Christmas" ? (
+                <div>Holiday: 🎄</div>
+            ) : null}
+        </div>
+    );
 }
